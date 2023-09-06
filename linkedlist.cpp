0188813eb0be80e1653efd64b9551a818f14096b
Yumilify <101920479+Yumilify@users.noreply.github.com>
2023-09-06 15:06:44 -0700

    Add files via upload

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -26,18 +26,52 @@ int main()
 
     head->Value = 5;
     head->Next = second; 
+
     second->Value = 6; 
     second->Next = third;
+
     third->Value = 7; 
     third->Next = fourth;
+
     fourth->Value = 8;
-    fourth->Next = fifth;
+    fourth->Next = fifth; 
+    
     fifth->Value = 9; 
     fifth->Next = six;
+
     six->Value = 10; 
     six->Next = NULL;
 
+    
+
     printList(head);
+
+
+    Node* pHead;
+    Node* pCurrent;
+    Node* pNext; 
+    
+    pHead = new Node();
+    pHead->Value = 1;
+    
+    pCurrent = new Node(); 
+    pCurrent->Value = 2;
+
+    pNext = new Node(); 
+    pNext->Value = 3; 
+
+    pHead->Next = pCurrent;
+    pCurrent->Next = NULL;
+
+    printList(pHead);
+
+
+    for (int i = 0; i < 10; i++) {
+
+
+    }
+
+
 
     return 0; 
 }
